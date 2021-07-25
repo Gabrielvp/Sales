8c1ff2eb84dded473214b35cd183d96b405a1f60
Gabriel <gabriel_0508@hotmail.com>
2021-07-25 09:18:42 -0300

    seeding service

---
+++ Sales/Data/SeedingService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sales.Models;
+using Sales.Models.Enums;
+
+namespace Sales.Data
+{
+    public class SeedingService
+    {
+        private SalesContext _context;
+
+        public SeedingService(SalesContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if(_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any())
+            {
+                return;
+            }
+
+            Department d1 = new Department(1, "Computers");
+            Department d2 = new Department(2, "Electronics");
+            Department d3 = new Department(3, "Fashion");
+            Department d4 = new Department(4, "Books");
+
+            Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
+            Seller s2 = new Seller(2, "Maria Green", "maria@gmail.com", new DateTime(1979, 12, 31), 3500.0, d2);
+            Seller s3 = new Seller(3, "Alex Grey", "alex@gmail.com", new DateTime(1988, 1, 15), 2200.0, d1);
+            Seller s4 = new Seller(4, "Martha Red", "martha@gmail.com", new DateTime(1993, 11, 30), 3000.0, d4);
+            Seller s5 = new Seller(5, "Donald Blue", "donald@gmail.com", new DateTime(2000, 1, 9), 4000.0, d3);
+            Seller s6 = new Seller(6, "Alex Pink", "pink@gmail.com", new DateTime(1997, 3, 4), 3000.0, d2);
+
+            SalesRecord r1 = new SalesRecord(1, new DateTime(2018, 09, 25), 11000.0, SaleStatus.billed, s1);
+            SalesRecord r2 = new SalesRecord(2, new DateTime(2018, 09, 23), 1000.0, SaleStatus.billed, s3);
+            SalesRecord r3 = new SalesRecord(3, new DateTime(2018, 09, 5), 1500.0, SaleStatus.pending, s4);
+            SalesRecord r4 = new SalesRecord(4, new DateTime(2018, 08, 5), 258000.0, SaleStatus.Canceled, s1);
+            SalesRecord r5 = new SalesRecord(5, new DateTime(2018, 08, 6), 18000.0, SaleStatus.billed, s2);
+            SalesRecord r6 = new SalesRecord(6, new DateTime(2018, 10, 1), 500.0, SaleStatus.billed, s2);
+            SalesRecord r7 = new SalesRecord(7, new DateTime(2018, 10, 13), 10.0, SaleStatus.billed, s6);
+            SalesRecord r8 = new SalesRecord(8, new DateTime(2018, 10, 13), 1500.0, SaleStatus.pending, s5);
+            SalesRecord r9 = new SalesRecord(9, new DateTime(2018, 09, 8), 238.25, SaleStatus.billed, s5);
+            SalesRecord r10 = new SalesRecord(10, new DateTime(2018, 1, 1), 1.32, SaleStatus.billed, s2);
+            SalesRecord r11 = new SalesRecord(11, new DateTime(2018, 2, 5), 11000.0, SaleStatus.Canceled, s1);
+            SalesRecord r12 = new SalesRecord(12, new DateTime(2018, 8, 26), 115.0, SaleStatus.Canceled, s3);
+            SalesRecord r13 = new SalesRecord(13, new DateTime(2018, 09, 13), 1100.0, SaleStatus.pending, s4);
+            SalesRecord r14 = new SalesRecord(14, new DateTime(2018, 09, 14), 11200.0, SaleStatus.billed, s6);
+            SalesRecord r15 = new SalesRecord(15, new DateTime(2018, 11, 12), 1304.0, SaleStatus.pending, s1);
+            SalesRecord r16 = new SalesRecord(16, new DateTime(2018, 11, 12), 110.0, SaleStatus.billed, s2);
+            SalesRecord r17 = new SalesRecord(17, new DateTime(2018, 11, 15), 348.0, SaleStatus.billed, s5);
+            SalesRecord r18 = new SalesRecord(18, new DateTime(2018, 12, 2), 37.0, SaleStatus.Canceled, s6);
+            SalesRecord r19 = new SalesRecord(19, new DateTime(2018, 12, 2), 111.32, SaleStatus.Canceled, s1);
+            SalesRecord r20 = new SalesRecord(20, new DateTime(2018, 12, 3), 147.50, SaleStatus.pending, s2);
+            SalesRecord r21 = new SalesRecord(21, new DateTime(2018, 12, 5), 1000.0, SaleStatus.pending, s3);
+            SalesRecord r22 = new SalesRecord(22, new DateTime(2018, 12, 7), 11000.0, SaleStatus.pending, s3);
+            SalesRecord r23 = new SalesRecord(23, new DateTime(2018, 12, 12), 52000.0, SaleStatus.billed, s2);
+            SalesRecord r24 = new SalesRecord(24, new DateTime(2018, 12, 12), 37000.0, SaleStatus.billed, s4);
+            SalesRecord r25 = new SalesRecord(25, new DateTime(2018, 12, 12), 11500.0, SaleStatus.Canceled, s1);
+            SalesRecord r26 = new SalesRecord(26, new DateTime(2018, 12, 16), 11500.0, SaleStatus.billed, s6);
+            SalesRecord r27 = new SalesRecord(27, new DateTime(2018, 12, 17), 12000.0, SaleStatus.pending, s5);
+            SalesRecord r28 = new SalesRecord(28, new DateTime(2018, 12, 18), 191000.0, SaleStatus.billed, s3);
+            SalesRecord r29 = new SalesRecord(29, new DateTime(2018, 12, 24), 11900.0, SaleStatus.billed, s2);
+            SalesRecord r30 = new SalesRecord(30, new DateTime(2018, 12, 25), 16.0, SaleStatus.billed, s1);
+
+            _context.Department.AddRange(d1, d2, d3, d4);
+            _context.Seller.AddRange(s1, s2, s3, s4, s5, s6);
+            _context.SalesRecord.AddRange(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30);
+
+            _context.SaveChanges();
+        }
+    }
+}
